c19e1fc152ab5c8aaec4adf6370227917557fbb6
16230091@qq.com <16230091@qq.com>
2017-05-23 16:51:10 +0800

    init

---
+++ MyWholeApp/HomePage/MainViewController.m
@@ -0,0 +1,57 @@
+//
+//  ZYRootViewController.m
+//  UINavigationControllerTest
+//
+//  Created by jax on 13-9-2.
+//  Copyright (c) 2013年 Bao. All rights reserved.
+//
+
+#import "MainViewController.h"
+#import "Navigator.h"
+
+@interface MainViewController () {
+    UIButton* module1Button;
+    UIButton* module2Button;
+}
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    module1Button = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 100,100)];
+    [module1Button setBackgroundColor: [UIColor redColor]];
+    [module1Button setTitle: @"Module1" forState:UIControlStateNormal];
+    [module1Button addTarget:self action:@selector(gotoModule1) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview: module1Button];
+
+    module2Button = [[UIButton alloc]initWithFrame:CGRectMake(200, 200, 100,100)];
+    [module2Button setBackgroundColor: [UIColor redColor]];
+    [module2Button setTitle: @"Module2" forState:UIControlStateNormal];
+    [module2Button addTarget:self action:@selector(gotoModule2) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview: module2Button];
+
+}
+
+- (void) gotoModule1 {
+    NSString* url = @"Module1ViewController";
+    NSMutableDictionary* data;
+    
+    [Navigator navigateTo:url withData: data];
+    
+//    [[NSNotificationCenter defaultCenter] postNotificationName:@"Module1" object:nil];
+}
+
+- (void) gotoModule2 {
+    NSString* url = @"Module2ViewController";
+    NSMutableDictionary* data = [[NSMutableDictionary alloc] init];
+    [data setValue: @"Jianqiang" forKey: @"UserName"];
+    [data setValue: @"28" forKey: @"Age"];
+    
+    [Navigator navigateTo:url withData: data];
+}
+
+@end
---
+++ MyWholeApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyWholeApp
+//
+//  Created by baojianqiang on 16/1/11.
+//  Copyright © 2016年 baojianqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
